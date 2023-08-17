85a14d585329b33917bd2016236ba6ac51a2e22c
mahmoudmohamed <mahmoudmohamed199885@gmail.com>
2023-08-17 15:20:23 +0300

    image

---
+++ storage/framework/views/03ac68ec69ff03336d3901e7590530b7.php
@@ -0,0 +1,8 @@
+<h2
+    <?php echo e($attributes->class(['filament-card-heading text-xl font-semibold tracking-tight'])); ?>
+
+>
+    <?php echo e($slot); ?>
+
+</h2>
+<?php /**PATH C:\laragon\www\awarebox-backend\vendor\filament\filament\src\/../resources/views/components/card/heading.blade.php ENDPATH**/ ?>
