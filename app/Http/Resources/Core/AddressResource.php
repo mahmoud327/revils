ef3cce56ad7c2c606de318d12724270f15f0cd23
mahmoudmohamed <mahmoudmohamed199885@gmail.com>
2023-08-28 10:11:05 +0300

    country_with_address

--- app/Http/Resources/Core/AddressResource.php
+++ app/Http/Resources/Core/AddressResource.php
@@ -19,9 +19,9 @@ class AddressResource extends JsonResource
         return [
             "id" => $this->id,
             "address" => $this->address,
-            "country_id" => $this->country_id,
-            "state_id" => $this->state_id,
-            "city_id" => $this->state_id,
+            "country" => new CityResource($this->whenLoaded('country')),
+            "city" => new CountryResource($this->whenLoaded('city')),
+            "state" => new StateResource($this->whenLoaded('state')),
             "note" => $this->note,
             "zipcode" => $this->zipcode,
             "address_type" => $this->address_type,
